77f5f2c5e592d0e6fb9634131f49a1f8da2cc3ae
Dmitry Kozlov <thaidem19@gmail.com>
2019-08-08 11:27:23 +0300

    Задание 3 / Сценарий логина

---
+++ java-example/src/test/java/ru/stqa/TestLogin.java
@@ -0,0 +1,33 @@
+package ru.stqa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestLogin {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testLogin() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
